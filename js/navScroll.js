1f68fc091c1e9baf3b97535f451040ccaf9c59f8
uthmana <uthmana.muhammad@gmail.com>
2018-03-12 14:26:04 +0200

    Add files via upload

---
+++ js/navScroll.js
@@ -0,0 +1,14 @@
+
+	<!-- transparent navbar scroll effect -->
+	     var nav = document.getElementById('nav');
+		 
+		 window.onscroll = function(){
+			 
+			 if(window.pageYOffset >100){
+				 nav.style.background="#0a7785";
+				 }
+			 else{
+				 nav.style.background = "transparent";
+				 }
+			 }
+	
